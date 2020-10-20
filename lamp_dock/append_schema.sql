4ac009de8649a9a964bb6de5f17e5cd3a24bdf7b
Asako Uemura <asakouemura3508@gmail.com>
2020-10-20 21:15:20 +0900

    table_design_re

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -2,11 +2,19 @@
 --
 
 CREATE TABLE `purchase_history` (
-  `history_id` int(11) NOT NULL,
-  `detail_id` int(11) NOT NULL,
+  `history_id` int(11) NOT NULL (Primary Key / Auto Increment),
   `user_id` int(11) NOT NULL,
+  `purchased_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- テーブルの構造 `purchase_detail`
+--
+
+CREATE TABLE `purchase_history` (
+  `detail_id` int(11) NOT NULL (Primary Key / Auto Increment),
+  `history_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
   `purchased_price` int(11) NOT NULL,
   `purchased_amount` int(11) NOT NULL,
-  `purchased_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
